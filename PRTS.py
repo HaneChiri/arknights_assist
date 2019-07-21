5609c3cdd9123cae6952a2f63e289952ddbfed42
HaneChiri <featherdust@126.com>
2019-07-21 11:25:30 +0800

    优化代码

--- PRTS.py
+++ PRTS.py
@@ -1,22 +1,10 @@
 import pyautogui
-import time
-
-start_point=(1700,950)#可以按到两个开始键的坐标
-start_point_color1=(0, 110, 160)#关卡选择的开始行动的颜色
-start_point_color2=(0, 98, 142)#编队选择的开始行动的颜色
-prts_point=(1600,880)#代理指挥开关处的白色部分
-prts_point_color=(255,255,255)
-
 
 start_game1='images\\start_game1.png'
 start_game2='images\\start_game2.png'
 prts_off='images\\PRTS_off.png'
 end_game='images\\end_game.png'
 
-def get_color(point=None):
-    if point==None:point=pyautogui.position()#默认为当前坐标
-    img=pyautogui.screenshot()
-    return img.getpixel(point)
 
 def get_point(image):
     p=pyautogui.locateOnScreen(image)
@@ -37,6 +25,7 @@ def open_ptrs():
 
 
 game_status=1
+game_num=0#已经完成的游戏局数
 
 while True:
     '''
@@ -61,28 +50,6 @@ while True:
         end_point=get_point(end_game)
         if end_point!=None:
             pyautogui.click(end_point)
-            print('[P.R.T.S]行动结束')
+            game_num+=1#游戏局数+1
+            print('[P.R.T.S]第{}次行动结束'.format(game_num))
             game_status=1
-    #print(game_status)
-
-    '''
-    if get_color(start_point)==start_point_color1:
-        pyautogui.moveTo(start_point)
-        pyautogui.click()
-        print('yes')
-        pass
-        print(get_color(start_point))
-        if get_color(start_point)==start_point_color2:
-            print('start')
-        else:
-            print('do not start')
-
-    else:
-        print('no')
-    '''
-    #time.sleep(1)
-
-
-
-
-#(1700,950)
