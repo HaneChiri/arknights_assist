89202c71a46c6bbfb9f1a4d2bfab920a7036168b
HaneChiri <featherdust@126.com>
2019-07-23 07:18:52 +0800

    代码格式化

--- PRTS.py
+++ PRTS.py
@@ -8,36 +8,37 @@
 
 import pyautogui
 
-#用于寻找按钮坐标的图片
-start_game1='images\\start_game1.png'
-start_game2='images\\start_game2.png'
-prts_off='images\\PRTS_off.png'
-end_game='images\\end_game.png'
+# 用于寻找按钮坐标的图片
+start_game1 = 'images\\start_game1.png'
+start_game2 = 'images\\start_game2.png'
+prts_off = 'images\\PRTS_off.png'
+end_game = 'images\\end_game.png'
 
 
 def get_point(image):
     '''
     获取image在屏幕上的中心坐标，如果未找到，则返回None
     '''
-    p=pyautogui.locateOnScreen(image)
-    if p!=None:
+    p = pyautogui.locateOnScreen(image)
+    if p != None:
         return pyautogui.center(p)
     else:
         return None
+
 
 def open_ptrs():
     '''
     打开代理指挥
     '''
-    p=get_point(prts_off)
-    if p!=None:#如果代理指挥是关着的
-        prts_point=p
+    p = get_point(prts_off)
+    if p != None:  # 如果代理指挥是关着的
+        prts_point = p
         pyautogui.click(prts_point)
         print('[P.R.T.S.]自动开启P.R.T.S')
 
 
-game_status=1
-game_num=1#已经完成的游戏局数
+game_status = 1
+game_num = 1  # 已经完成的游戏局数
 
 print('[P.R.T.S.]本系统为代理指挥的代理指挥，用于帮助博士在一次代理指挥结束后，自动启动下一次代理指挥作战')
 print('[P.R.T.S.]本系统由Dr.憧憬少#1847制作')
@@ -48,31 +49,31 @@ print('[P.R.T.S.]代理指挥作战正常运行中...在本界面使用Ctrl+C终
 
 try:
     while True:
-        
+
         '''
         if game_status==0:
             open_ptrs()
             game_status=1
         '''
-        if game_status==1:#关卡选择
-            start_point=get_point(start_game1)
-            if start_point!=None:
+        if game_status == 1:  # 关卡选择
+            start_point = get_point(start_game1)
+            if start_point != None:
                 pyautogui.click(start_point)
                 print('[P.R.T.S.]关卡选择完毕')
-                game_status=2
-        elif game_status==2:#编队选择
-            start_point=get_point(start_game2)
-            if start_point!=None:
+                game_status = 2
+        elif game_status == 2:  # 编队选择
+            start_point = get_point(start_game2)
+            if start_point != None:
                 pyautogui.click(start_point)
                 print('[P.R.T.S.]编队选择完毕，开始第{}次行动！'.format(game_num))
-                game_status=3
-        elif game_status==3:#游戏结束
-            end_point=get_point(end_game)
-            if end_point!=None:
+                game_status = 3
+        elif game_status == 3:  # 游戏结束
+            end_point = get_point(end_game)
+            if end_point != None:
                 pyautogui.click(end_point)
                 print('[P.R.T.S.]第{}次行动结束'.format(game_num))
-                game_num+=1#游戏局数+1
-                game_status=1
+                game_num += 1  # 游戏局数+1
+                game_status = 1
 except FileNotFoundError:
     print('[P.R.T.S.]代理指挥出现严重失误')
     print('[P.R.T.S.]请检查exe文件路径下是否存在images文件夹，且文件夹内是否存在以下图片：')
